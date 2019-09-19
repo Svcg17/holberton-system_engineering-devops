10deea15b68f82253a027b30275fe8a891acff5a
Sofia Cheung <sofiavictoria17@gmail.com>
2019-09-19 20:59:19 +0000

    Changing value error to exception

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,11 +10,13 @@ def number_of_subscribers(subreddit):
     queries the Reddit API and returns the number of subscribers
     for a given subrredit
     """
+    if (subreddit):
+        req = requests.get('https://www.reddit.com/r/{}/about.json'
+                           .format(subreddit),
+                           headers={'User-agent': 'My User Agent 1.0'},
+                           allow_redirects=False)
     try:
-        return (requests.get('https://www.reddit.com/r/{}/about.json'
-                             .format(subreddit),
-                             headers={'User-agent': 'My User Agent 1.0'},
-                             allow_redirects=False)
-                .json().get('data').get('subscribers'))
-    except ValueError:
+        subs = (req.json().get('data').get('subscribers'))
+        return (subs)
+    except Exception:
         return (0)
