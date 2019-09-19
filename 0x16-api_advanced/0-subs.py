fcc52da4945efbcfd479186ce8426314a637a177
Sofia Cheung <sofiavictoria17@gmail.com>
2019-09-19 20:51:40 +0000

    refactoring code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,12 +10,11 @@ def number_of_subscribers(subreddit):
     queries the Reddit API and returns the number of subscribers
     for a given subrredit
     """
-    req = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit),
-                       headers={'User-agent': 'My User Agent 1.0'},
-                       allow_redirects=False)
     try:
-        subs = (req.json().get('data').get('subscribers'))
-        return (subs)
+        return (requests.get('https://www.reddit.com/r/{}/about.json'
+                             .format(subreddit),
+                             headers={'User-agent': 'My User Agent 1.0'},
+                             allow_redirects=False)
+                .json().get('data').get('subscribers'))
     except ValueError:
         return (0)
