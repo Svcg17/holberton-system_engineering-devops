4ffe16d3e68248c61783fe002b482c8458c16083
Sofia Cheung <sofiavictoria17@gmail.com>
2019-09-20 04:08:59 +0000

    Adding task 2 scripts

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+The 2-recurse module
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """
+    Recursive function that queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit.
+    """
+    req = requests.get('https://www.reddit.com/r/{}/hot.json?after={}'
+                       .format(subreddit, after),
+                       headers={'User-agent': 'My User Agent 1.0'},
+                       allow_redirects=False)
+    if req.status_code != 200:
+        return None
+    if after is None:
+        return hot_list
+
+    for i in req.json().get('data').get('children'):
+        hot_list.append(i.get('data').get('title'))
+
+    after = req.json().get('data').get('after')
+
+    return recurse(subreddit, hot_list, after)
